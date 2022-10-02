514332c6f8b33653a1cd217ebd3c1c60d4c73e09
theJoystick26 <adinjj@gmail.com>
2022-10-02 01:51:10 -0400

    Cell UI

---
+++ PillBuddy/Medication.swift
@@ -0,0 +1,14 @@
+//
+//  Medication.swift
+//  PillBuddy
+//
+//  Created by Adin Joyce on 10/1/22.
+//
+
+import Foundation
+
+struct Medication {
+    var name: String
+    var dosage: Int
+    var daysLeft: Int
+}
